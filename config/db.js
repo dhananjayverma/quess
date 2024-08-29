de564614f76b1006182277636345a4d92fe0810d
Dhananjay kumar verma <dhananjaykumarverma088@gmail.com>
2024-08-29 14:58:21 +0530

    update

--- config/db.js
+++ config/db.js
@@ -1,28 +1,13 @@
-// const mongoose = require('mongoose');
-
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGO_URI, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//     console.log('MongoDB Connected');
-//   } catch (err) {
-//     console.error(err.message);
-//     process.exit(1);
-//   }
-// };
-
-// module.exports = connectDB;
-
-
-
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
+    // Connect to MongoDB
     await mongoose.connect(process.env.MONGO_URI, {
-      serverSelectionTimeoutMS: 30000 // 30 seconds
+      useNewUrlParser: true, // New URL parser to handle connection strings
+      useUnifiedTopology: true, // Use the new Server Discover and Monitoring engine
+      serverSelectionTimeoutMS: 30000, // 30 seconds for server selection timeout
+      socketTimeoutMS: 45000 // 45 seconds for socket timeout
     });
     console.log('MongoDB connected');
   } catch (error) {
